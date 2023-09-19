87850616f039d77e3589b24629c59be50cb6dc07
EBusch1 <elijah.t.busch@gmail.com>
2023-09-19 01:32:17 -0500

    Sending Emails

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 # Day 31 SMTP DateTime
 
+import smtplib
+
+
+
+# gmail_connection = smtplib.SMTP("smtp.gmail.com")
+with smtplib.SMTP("smtp.gmail.com") as gmail_connection:
+    gmail_connection.starttls()
+    gmail_connection.login(user=my_email, password=my_password)
+    gmail_connection.sendmail(from_addr=my_email, to_addrs=their_email, msg="Subject: A greeting\n\n Howdy from the "
+                                                                            "body!")
+# gmail_connection.close() # This setup automatically closes.
